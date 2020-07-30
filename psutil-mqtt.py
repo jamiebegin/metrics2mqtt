00d6620744366d313e69aee966817fb79b6b9257
Jamie Begin <jjbegin@gmail.com>
2020-07-30 13:59:52 -0400

    Broke metrics out into seperate objects

--- psutil-mqtt.py
+++ psutil-mqtt.py
@@ -21,11 +21,67 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 fh.setFormatter(formatter)
 logger.addHandler(fh)
 '''
+
+class PSUtilMetric(object):
+    def __init__(self, *args, **kwargs):
+        self.icon = "mdi:desktop-tower-monitor"
+        self.unit_of_measurement = "%"
+        self.topics = None
+
+    def get_config_topic(self, topic_prefix, system_name):
+        def sanitize(val):
+            return val.lower().replace(" ", "_")
+        sn = sanitize(system_name)
+        n = sanitize(self.name)
+        t = {}
+        t['state'] = "{}/sensor/{}/{}/state".format(topic_prefix, sn, n)
+        t['config'] = "{}/sensor/{}/{}/config".format(topic_prefix, sn, n)
+        t['avail'] = "{}/sensor/{}/{}/availability".format(topic_prefix, sn, n)
+        t['attrs'] = "{}/sensor/{}/{}/attributes".format(topic_prefix, sn, n)
+        self.topics = t
+        
+        config_topic = {'name': system_name + ' ' + self.name,
+            'unique_id': sn + '_' + n,
+            'qos': 1,
+            'icon': self.icon,
+            'unit_of_measurement': self.unit_of_measurement,
+            'availability_topic': t['avail'],
+            'json_attributes_topic': t['attrs'],
+            'state_topic': t['state']}
+        return config_topic
+
+class CPUMetric(PSUtilMetric):
+    def __init__(self, *args, **kwargs):
+        super(CPUMetric, self).__init__(*args, **kwargs)
+        self.name = "CPU"
+        self.icon = "mdi:chip"
+
+    def get_state(self):
+        r = {}
+        cpu_times = psutil.cpu_times_percent(interval=5, percpu=False)
+        r['state'] = "{:.1f}".format(100.0 - cpu_times.idle)
+        r['attrs'] = jsons.dump(cpu_times)
+        return r
+
+class VirtualMemoryMetric(PSUtilMetric):
+    def __init__(self, *args, **kwargs):
+        super(VirtualMemoryMetric, self).__init__(*args, **kwargs)
+        self.name = "Virtual Memory"
+        self.icon = "mdi:memory"
+
+    def get_state(self):
+        r = {}
+        vm = psutil.virtual_memory()
+        r['state'] = "{:.1f}".format(vm.percent)
+        r['attrs'] = jsons.dump(vm)
+        return r
+
 class PSUtilDaemon(object):
-    def __init__(self, system_name, broker_host, topics):
+    def __init__(self, system_name, broker_host, topic_prefix):
         self.system_name = system_name
         self.broker_host = broker_host
-        self.topics = topics
+        self.topic_prefix = topic_prefix
+        self.metrics = []
         
         signal.signal(signal.SIGTERM, self.sig_handle)
         signal.signal(signal.SIGINT, self.sig_handle)
@@ -35,60 +91,55 @@ class PSUtilDaemon(object):
         try: 
               self.client.connect(self.broker_host)
               logger.info("Connected to MQTT broker.")
-              self._report_status(True)
               self.client.loop_start()
         except Exception as e:
               logger.error("Error while trying to connect to MQTT broker.")
               logger.error(str(e))
               raise
 
-    def _report_status(self, status):
+    def _report_status(self, status, avail_topic):
         if status: status = 'online'
         else: status = 'offline'
-        logger.debug('Publishing "{}" to {}'.format(status, self.topics['avail']))
-        self.client.publish(self.topics['avail'], status, retain=True, qos=1)
+        logger.debug('Publishing "{}" to {}'.format(status, avail_topic))
+        self.client.publish(avail_topic, status, retain=True, qos=1)
 
     def sig_handle(self, signum, frame):
         self._cleanup(0)
 
     def _cleanup(self, exit_code=0):
         logger.warning("Shutting down gracefully.")
-        self._report_status(False)
+        for metric in self.metrics:
+            self._report_status(False, metric.topics['avail'])
         self.client.loop_stop() 
         self.client.disconnect()
         sys.exit(exit_code)
 
-    def config(self, exit=False):
-        p = {'name': self.system_name + ' CPU',
-        'unique_id': self.system_name + '_cpu',
-        'qos': 1,
-        'icon': 'mdi:chip',
-        'unit_of_measurement': '%',
-        'availability_topic': self.topics['avail'],
-        'json_attributes_topic': self.topics['attrs'],
-        'state_topic': self.topics['state']}
-        self.client.publish(self.topics['config'], json.dumps(p), retain=True, qos=1)
-        if exit:
-            logger.critical("Exiting after creating new topic: {}".format(p))  
-            self._cleanup()
+    def create_config_topics(self):
+        for metric in self.metrics:
+            config_topic = metric.get_config_topic(self.topic_prefix, self.system_name)
+            print(json.dumps(config_topic))
+            self.client.publish(metric.topics['config'], json.dumps(config_topic), retain=True, qos=1)
 
     def clean(self):
         raise NotImplementedError("Clean function doesn't work yet.")
 
-    def cpu(self):
-        cpu_times = psutil.cpu_times_percent(interval=60, percpu=False)
-        new_state = "{:.1f}".format(100.0 - cpu_times.idle)
-        attrs = jsons.dump(cpu_times)
-        logger.debug("Publishing '{}' to {}".format(json.dumps(new_state), self.topics['state']))
-        self.client.publish(self.topics['state'], new_state, retain=False, qos=1)
-        logger.debug("Publishing '{}' to {}".format(json.dumps(attrs), self.topics['attrs']))
-        self.client.publish(self.topics['attrs'], json.dumps(attrs), retain=False, qos=1)
+    def add_metric(self, metric):
+        self.metrics.append(metric)
 
     def monitor(self):
-        self.config()
-            while True:
-                self.cpu()
+        self.create_config_topics()
+        for metric in self.metrics:
+            self._report_status(True, metric.topics['avail'])
 
+        while True:
+            for metric in self.metrics:
+                s = metric.get_state()
+                state = s['state']
+                attrs = json.dumps(s['attrs'])
+                logger.debug("Publishing '{}' to topic '{}'.".format(state, metric.topics['state']))
+                self.client.publish(metric.topics['state'], state, retain=False, qos=1)
+                logger.debug("Publishing '{}' to topic '{}'.".format(attrs, metric.topics['attrs']))
+                self.client.publish(metric.topics['attrs'], attrs, retain=False, qos=1)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -97,22 +148,20 @@ if __name__ == "__main__":
    
     args = parser.parse_args()
 
-    system_name = 'nuc'
-    metric = 'cpu'
+    system_name = 'NUC'
     broker_host = "192.168.7.60"
     topic_prefix = "homeassistant"
-    topics = {}
-    topics['state'] = "{}/sensor/{}/{}/state".format(topic_prefix, system_name, metric)
-    topics['config'] = "{}/sensor/{}/{}/config".format(topic_prefix, system_name, metric)
-    topics['avail'] = "{}/sensor/{}/{}/availability".format(topic_prefix, system_name, metric)
-    topics['attrs'] = "{}/sensor/{}/{}/attributes".format(topic_prefix, system_name, metric)
 
-    stats = PSUtilDaemon(system_name, broker_host, topics)
+    stats = PSUtilDaemon(system_name, broker_host, topic_prefix)
+    cpu = CPUMetric()
+    stats.add_metric(cpu)
+    vm = VirtualMemoryMetric()
+    stats.add_metric(vm)
     stats.connect()
 
     if args.clean:
         stats.clean()
     elif args.config:
-        stats.config(exit=True)
+        stats.create_config_topic(exit=True)
     else:
         stats.monitor()
